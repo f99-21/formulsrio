0b185ad8a5c91f126d9a28608a2e112bf799d19b
Fabricio Sandoval <sandovalfabri2023@gmail.com>
2025-03-28 09:41:55 -0600

    cambiao

--- formulsrio/Controllers/MarcasController.cs
+++ formulsrio/Controllers/MarcasController.cs
@@ -9,40 +9,15 @@ namespace formulsrio.Controllers
 {
     public class MarcasController : Controller
     {
-        /**
-        private readonly equiposDbContext _context;
-
-        public MarcasController(equiposDbContext context)
-        {
-            _context = context;
-        }
+       
 
         public IActionResult Index()
         {
-            var listaDeMarcas = (from m in _context.marcas
-                                 select m).ToList();
-            ViewData["listadoDeMarcas"] = new SelectList(listaDeMarcas, "id_marcas", "nombre_marca");
-
-            var listadoDeEquipos = (from e in _context.equipos
-                                    join m in _context.marcas on e.marca_id equals m.id_marcas
-                                    select new
-                                    {
-                                        nombre = e.nombre,
-                                        descripcion = e.descripcion,
-                                        marcas_id = m.id_marcas,
-                                        marca_nombre = m.nombre_marca,
-                                    }).ToList();
-
-            ViewData["listadoEquipo"] = listadoDeEquipos;
+         
             return View();
         }
 
-        public IActionResult CrearEquipo(equipos nuevoEquipo)
-        {
-            _context.Add(nuevoEquipo);
-            _context.SaveChanges();
-            return RedirectToAction("Index");
-        }**/
+      
         
     }
 }
